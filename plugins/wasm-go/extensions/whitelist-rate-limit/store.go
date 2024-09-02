a623f2bc3c02fa7629e18adefb422ee7c29460df
zijiren233 <pyh1670605849@gmail.com>
2024-09-02 16:21:46 +0800

    refactor: use lru

--- plugins/wasm-go/extensions/whitelist-rate-limit/store.go
+++ plugins/wasm-go/extensions/whitelist-rate-limit/store.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"sync"
 	"time"
 )
@@ -9,11 +10,11 @@ type store struct {
 	tokens   uint64
 	interval time.Duration
 
-	sweepInterval time.Duration
-	sweepMinTTL   uint64
+	capacity int
 
-	data     map[string]*bucket
+	data     map[string]*list.Element
 	dataLock sync.RWMutex
+	lruList  *list.List
 }
 
 type storeConfig func(*store)
@@ -30,81 +31,62 @@ func withInterval(interval time.Duration) storeConfig {
 	}
 }
 
-func withSweepInterval(interval time.Duration) storeConfig {
+func withCapacity(capacity int) storeConfig {
 	return func(s *store) {
-		s.sweepInterval = interval
-	}
-}
-
-func withSweepMinTTL(ttl time.Duration) storeConfig {
-	return func(s *store) {
-		s.sweepMinTTL = uint64(ttl)
-	}
-}
-
-func withInitialAlloc(alloc int) storeConfig {
-	return func(s *store) {
-		s.data = make(map[string]*bucket, alloc)
+		s.capacity = capacity
 	}
 }
 
 func newStore(conf ...storeConfig) *store {
 	s := &store{
-		tokens:        1,
-		interval:      time.Second,
-		sweepInterval: 5 * time.Minute,
-		sweepMinTTL:   uint64(10 * time.Second),
+		tokens:   1,
+		interval: time.Second,
+		capacity: 1000,
+		data:     make(map[string]*list.Element),
+		lruList:  list.New(),
 	}
 
 	for _, c := range conf {
 		c(s)
 	}
 
-	go s.purge()
 	return s
 }
+
 func (s *store) Take(key string) (uint64, uint64, uint64, bool) {
 	s.dataLock.RLock()
-	if b, ok := s.data[key]; ok {
+	if elem, ok := s.data[key]; ok {
+		s.lruList.MoveToFront(elem)
+		b := elem.Value.(*bucket)
 		s.dataLock.RUnlock()
 		return b.take(s.tokens)
 	}
 	s.dataLock.RUnlock()
 
 	s.dataLock.Lock()
-	if b, ok := s.data[key]; ok {
+	if elem, ok := s.data[key]; ok {
+		s.lruList.MoveToFront(elem)
+		b := elem.Value.(*bucket)
 		s.dataLock.Unlock()
 		return b.take(s.tokens)
 	}
 
 	b := newBucket(s.tokens, s.interval)
+	elem := s.lruList.PushFront(b)
+	s.data[key] = elem
 
-	s.data[key] = b
+	if s.lruList.Len() > s.capacity {
+		oldest := s.lruList.Back()
+		s.lruList.Remove(oldest)
+		delete(s.data, oldest.Value.(*bucket).key)
+	}
+
 	s.dataLock.Unlock()
 	return b.take(s.tokens)
 }
 
-func (s *store) purge() {
-	ticker := time.NewTicker(s.sweepInterval)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		s.dataLock.Lock()
-		now := uint64(time.Now().UnixNano())
-		for k, b := range s.data {
-			b.lock.Lock()
-			lastTime := b.startTime + (b.lastTick * uint64(b.interval))
-			b.lock.Unlock()
-
-			if now-lastTime > s.sweepMinTTL {
-				delete(s.data, k)
-			}
-		}
-		s.dataLock.Unlock()
-	}
-}
-
 type bucket struct {
+	key             string
 	startTime       uint64
 	interval        time.Duration
 	availableTokens uint64
